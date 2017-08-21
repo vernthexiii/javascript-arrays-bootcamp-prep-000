cd283fb5e0b0b6a16f5d6c9efafdea814266e197
Drew D <vernthexiii@gmail.com>
2017-08-21 19:04:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,6 +17,8 @@ function addElementToEndOfArray(array, element) {
 
 function destructivelyAddElementToEndOfArray(array, element) {
   //alters input array and returns result
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
