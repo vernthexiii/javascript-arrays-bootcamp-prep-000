0481ff8493fd646bf366a110f9174eccd790ca92
Drew D <vernthexiii@gmail.com>
2017-08-21 18:58:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,37 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
-  var newArray = [element];
-  newarray.concat(array);
-  return newArray;
+  //does not alter input array returns new array
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  //alters input array and returns result
+}
+
+function addElementToEndOfArray(array, element) {
+  //does not alter input array returns new array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  //alters input array and returns result
+}
+
+function accessElementInArray(array, index) {
+  //returns element at specific index
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  //alters input array and returns result
+}
+
+function removeElementFromBeginningOfArray(array) {
+  
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  
+}
+
+function removeElementFromEndOfArray(array) {
+  
 }
