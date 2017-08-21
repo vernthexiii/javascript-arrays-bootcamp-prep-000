a9bbd010d95508491c1ddc5b290a0c681c28ff8f
Drew D <vernthexiii@gmail.com>
2017-08-21 19:09:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,8 +33,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-array.slice(0);
-return array;
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
