39a13cac87ee2d3e041b91b391a4792ef316b726
Drew D <vernthexiii@gmail.com>
2017-08-21 19:04:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 
 function accessElementInArray(array, index) {
   //returns element at specific index
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
